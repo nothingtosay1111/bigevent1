8b33e4f45d7ce13272f4e2fdc759ddea07cb1af9
nothingtosay1111 <c18734734863@163.com>
2021-02-21 09:14:07 +0800

    资源管理

--- bigevent-test-fe-static/assets/js/swiper/swiper.js
+++ bigevent-test-fe-static/assets/js/swiper/swiper.js
@@ -1,83 +1,75 @@
-$(function () {
-  // 轮播图
-  var form = layui.form
+$(function() {
+    var form = layui.form
 
-  // 获取轮播图列表数据
-  function loadSwiperList () {
-    $.ajax({
-      type: 'get',
-      url: 'admin/swipers',
-      success: function (res) {
-        var tags = template('table-tpl', res)
-        $('.layui-table tbody').html(tags)
-      }
+    function loadSwiperList() {
+        $.ajax({
+            type: 'get',
+            url: 'admin/swipers',
+            success: function(res) {
+                var tags = template('table-tpl', res)
+                $('.layui-table tbody').html(tags)
+            }
+        })
+    }
+
+    loadSwiperList();
+
+    $('.layui-table tbody').on('click', '.layui-badge', function(e) {
+        let status = $(e.target).data('status')
+        let id = $(e.target).data('id')
+        $.ajax({
+            type: 'put',
+            url: 'admin/swipers/' + id,
+            data: {
+                status: status
+            },
+            success: function(res) {
+                if (res.status === 0) {
+                    layer.msg(res.message)
+                    loadSwiperList()
+                }
+            }
+        })
     })
-  }
 
-  loadSwiperList()
 
-  // 轮播图状态切换
-  $('.layui-table tbody').on('click', '.layui-badge', function (e) {
-    let status = $(e.target).data('status')
-    let id = $(e.target).data('id')
-    $.ajax({
-      type: 'put',
-      url: 'admin/swipers/' + id,
-      data: {
-        status: status
-      },
-      success: function (res) {
-        if (res.status === 0) {
-          // 切换成功
-          layer.msg(res.message)
-          loadSwiperList()
-        }
-      }
+    $('.layui-table tbody').on('click', '.delete', function(e) {
+        var id = $(e.target).data('id')
+        layer.confirm('确实要删除吗？', function(index) {
+            $.ajax({
+                type: 'delete',
+                url: 'admin/swipers/' + id,
+                success: function(res) {
+                    if (res.status === 0) {
+                        layer.close(index)
+                        loadSwiperList()
+                    }
+                }
+            })
+        })
     })
-  })
 
-  // 删除操作
-  $('.layui-table tbody').on('click', '.delete', function (e) {
-    var id = $(e.target).data('id')
-    layer.confirm('确实要删除吗？', function (index) {
-      $.ajax({
-        type: 'delete',
-        url: 'admin/swipers/' + id,
-        success: function (res) {
-          if (res.status === 0) {
-            // 删除成功
-            layer.close(index)
-            loadSwiperList()
-          }
-        }
-      })
+    $('#uploadSwiper').click(function() {
+        $('#myfile').click()
     })
-  })
-
-  // 上传轮播图按钮点击事件
-  $('#uploadSwiper').click(function () {
-    $('#myfile').click()
-  })
-
-  // 监听文件选中事件
-  $('#myfile').change(function (e) {
-    let files = e.target.files
-    var fd = new FormData()
-    $.each(files, function (index, item) {
-      fd.append('swipers', item)
+    $('#myfile').change(function(e) {
+        let files = e.target.files
+        var fd = new FormData()
+        $.each(files, function(index, item) {
+            fd.append('swipers', item)
+        })
+        $.ajax({
+            type: 'post',
+            url: 'admin/swipers',
+            data: fd,
+            processData: false,
+            contentType: false,
+            success: function(res) {
+                if (res.status === 0) {
+                    layer.msg(res.message)
+                    loadSwiperList()
+                }
+            }
+        })
     })
-    $.ajax({
-      type: 'post',
-      url: 'admin/swipers',
-      data: fd,
-      processData: false,
-      contentType: false,
-      success: function (res) {
-        if (res.status === 0) {
-          layer.msg(res.message)
-          loadSwiperList()
-        }
-      }
-    })
-  })
 })
